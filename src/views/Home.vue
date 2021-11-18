1eb5690513e5fc2495bf9ffdbafdffd5064d886c
André <salesmendesandre@gmail.com>
2021-11-18 17:21:33 +0100

    Act

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,26 @@
 <template>
   <v-container>
     <v-row cols="12">
-      <v-col   cols="12" md="6"   >
+      <v-col cols="12" md="6">
         <img src="/portada.svg" width="60%" style="margin-left: 15%">
       </v-col>
 
       <v-col cols="12" md="6">
         <div class="text-portada">
-          <b><span style="color:#1170b7">WORDS<span style="color:#D35B31">4</span>ALL</span></b> is simply dummy text of
-          the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the
-          1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has
-          survived not only five centuries, but also the leap into electronic typesetting, remaining essentially
-          unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
-          passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem
-          Ipsum.
+          Las palabras son los cimientos de la educación, que es el motor de las oportunidades y la vía para conseguir
+          la igualdad social. El desarrollo de tecnologías que permitan la gamificación y dinamización permite unir la
+          cultura digital y la educación.
+          <br>
+          <br>
+          En este desafío educativo llamado <b><span style="color:#1170b7">WORDS<span style="color:#D35B31">4</span>ALL</span></b> se presentan niveles y temáticas variadas. Que guiarán al usuario en una experiencia
+          virtual educativa interactiva que reforzara desde la cultura general a conocimientos de materias más
+          específicas. Tendrás que responder las 25 letras correctamente para llegar al objetivo.
 
           <div style="margin-top: 50px">
-            <v-btn block color="primary" rounded large @click="$router.push({name:'Categories'})"> Empezar a aprender</v-btn>
-            </div>
-            <div style="margin-top: 10px">
+            <v-btn block color="primary" rounded large @click="$router.push({name:'Categories'})"> Empezar a aprender
+            </v-btn>
+          </div>
+          <div style="margin-top: 10px">
             <v-btn block color="primary" rounded large @click="$router.push({name:'Team'})"> Equipo</v-btn>
           </div>
 
@@ -28,7 +30,7 @@
     </v-row>
 
     <v-row>
-      <v-col cols="12"  style="margin-top: 100px">
+      <v-col cols="12" style="margin-top: 100px">
         <img src="/logos.png" width="60%" style="margin-left: 20%;">
       </v-col>
     </v-row>
@@ -37,8 +39,7 @@
 
 <script>
 
-export default {
-}
+export default {}
 </script>
 
 <style>
