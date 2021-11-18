8d2e584c9bcaa29cecce141727e2afc9f70ee0af
André <salesmendesandre@gmail.com>
2021-11-18 14:55:57 +0100

    Questions

--- src/views/categories.js
+++ src/views/categories.js
@@ -4,44 +4,185 @@ const categories = [
         title: 'Básico',
         desc: '...',
         img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZk-eHFfHWf_f6JdBiz2ijs3I6-W2iRFhmqj15qm56hp75ck4MokytOzREsurjqBO3d1c&usqp=CAU',
-
+        questions: [
+            {
+                text0: 'Para la letra A',
+                text: 'Planta comestible con tallos blancos y hojas verdes. También está en la ducha.',
+                character: 'A',
+                answer: 'alcachofa',
+                status: 'unsolved'
+            },
+            {
+                text0: 'Para la letra B',
+                text: 'Caja en la que se guardan medicinas.',
+                character: 'B',
+                answer: 'botiquín',
+                status: 'unsolved'
+            },
+            {
+                text0: 'Para la letra C',
+                text: 'Cama en la que se llevan de un lado a otro heridos o enfermos.',
+                character: 'C',
+                answer: 'camilla',
+                status: 'unsolved'
+            },
+            {
+                text0: 'Para la letra D',
+                text: 'Palabras que se escriben y se dicen como regalo a alguien',
+                character: 'D',
+                answer: 'dedicatoria',
+                status: 'unsolved'
+            },
+            {
+                text0: 'Para la letra E',
+                text: 'Mancharse un cristal con el vapor de agua.',
+                character: 'E',
+                answer: 'empañar',
+                status: 'unsolved'
+            },
+            {
+                text0: 'Para la letra F',
+                text: 'Cuento en el que los personajes son animales, con el que se aprende algo através de una moraleja.',
+                character: 'F',
+                answer: 'fábula',
+                status: 'unsolved'
+            },
+            {
+                text0: 'Para la letra G',
+                text: 'Preparar alimentos cocinándolos con calor',
+                character: 'G',
+                answer: 'guisar',
+                status: 'unsolved'
+            },
+            {
+                text0: 'Para la letra H',
+                text: 'Tela resistente que se cuelga de sus extremos y se utiliza como cama.',
+                character: 'H',
+                answer: 'hamaca',
+                status: 'unsolved'
+            },
+            {
+                text0: 'Para la letra I',
+                text: 'Alfiler que se abrocha quedando su punta dentro de un gancho.',
+                character: 'I',
+                answer: 'imperdible',
+                status: 'unsolved'
+            },
+            {
+                text0: 'Para la letra J',
+                text: 'Persona que monta a caballo.',
+                character: 'J',
+                answer: 'jinete',
+                status: 'unsolved'
+            },
+            {
+                text0: 'Para la letra L',
+                text: 'Mueble formado por dos camas puestas una encima de la otra.',
+                character: 'L',
+                answer: 'litera',
+                status: 'unsolved'
+            },
+            {
+                text0: 'Para la letra M',
+                text: 'Salsa que se hace batiendo huevo y aceite.',
+                character: 'M',
+                answer: 'mayonesa',
+                status: 'unsolved'
+            },
+            {
+                text0: 'Para la letra N',
+                text: 'Parte posterior de la cabeza situada encima del cuello.',
+                character: 'N',
+                answer: 'nuca',
+                status: 'unsolved'
+            },
+            {
+                text0: 'Para la letra O',
+                text: 'Borde del mar, de un lago o de un río.',
+                character: 'O',
+                answer: 'orilla',
+                status: 'unsolved'
+            },
+            {
+                text0: 'Para la letra P',
+                text: 'Quitar las arrugar a una prenda.',
+                character: 'P',
+                answer: 'planchar',
+                status: 'unsolved'
+            },
+            {
+                text0: 'Para la letra Q',
+                text: 'Producto natural o preparado que sirve para quitar manchas.',
+                character: 'Q',
+                answer: 'quitamanchas',
+                status: 'unsolved'
+            },
+            {
+                text0: 'Para la letra R',
+                text: 'Conjunto de uvas sostenidas en un mismo tallo.',
+                character: 'R',
+                answer: 'racimo',
+                status: 'unsolved'
+            },
+            {
+                text0: 'Para la letra S',
+                text: 'Enterrar semillas en la tierra para que crezcan',
+                character: 'S',
+                answer: 'sembrar',
+                status: 'unsolved'
+            },
+            {
+                text0: 'Para la letra T',
+                text: 'Asiento sin respaldo',
+                character: 'T',
+                answer: 'taburete',
+                status: 'unsolved'
+            },
+            {
+                text0: 'Para la letra U',
+                text: 'Extender mantequilla sobre una rebanada de pan.',
+                character: 'U',
+                answer: 'untar',
+                status: 'unsolved'
+            },
+            {
+                text0: 'Para la letra V',
+                text: 'Persona que vive en el mismo barrio o edificio que otra.',
+                character: 'V',
+                answer: 'vecino',
+                status: 'unsolved'
+            },
+            {
+                text0: 'Para la letra W',
+                text: 'Sistema de conexión inalámbrica para conectarse a internet.',
+                character: 'W',
+                answer: 'wifi',
+                status: 'unsolved'
+            },
+            {
+                text0: 'Contiene la X',
+                text: 'Ave fabulosa que los antiguos creyeron que era única y renacía de sus cenizas.',
+                character: 'X',
+                answer: 'fénix',
+                status: 'unsolved'
+            },
+            {
+                text0: 'Para la letra Y',
+                text: 'Hueso que se encuentra dentro del oído, situado entre el martillo y el estribo. ',
+                character: 'Y',
+                answer: 'yunque',
+                status: 'unsolved'
+            },
+            {
+                text0: 'Para la letra Z',
+                text: 'Sonido que producen algunos insectos como la abeja o el mosquito.',
+                character: 'Z',
+                answer: 'zumbido',
+                status: 'unsolved'
+            }
+        ]
     },
-    {
-        id: 2,
-        title: 'Básico',
-        desc: '...',
-        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZk-eHFfHWf_f6JdBiz2ijs3I6-W2iRFhmqj15qm56hp75ck4MokytOzREsurjqBO3d1c&usqp=CAU',
-
-    },
-    {
-        id: 3,
-        title: 'Básico',
-        desc: '...',
-        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZk-eHFfHWf_f6JdBiz2ijs3I6-W2iRFhmqj15qm56hp75ck4MokytOzREsurjqBO3d1c&usqp=CAU',
-
-    },
-    {
-        id: 4,
-        title: 'Básico',
-        desc: '...',
-        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZk-eHFfHWf_f6JdBiz2ijs3I6-W2iRFhmqj15qm56hp75ck4MokytOzREsurjqBO3d1c&usqp=CAU',
-
-    },
-    {
-        id: 5,
-        title: 'Básico',
-        desc: '...',
-        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZk-eHFfHWf_f6JdBiz2ijs3I6-W2iRFhmqj15qm56hp75ck4MokytOzREsurjqBO3d1c&usqp=CAU',
-
-    },
-    {
-        id: 6,
-        title: 'Básico',
-        desc: '...',
-        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZk-eHFfHWf_f6JdBiz2ijs3I6-W2iRFhmqj15qm56hp75ck4MokytOzREsurjqBO3d1c&usqp=CAU',
-
-    }
-    ]
+e]
 
 
 module.exports = categories;
